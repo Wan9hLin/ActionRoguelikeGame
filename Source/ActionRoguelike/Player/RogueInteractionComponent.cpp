ef3aec3111b41bce756326c3f6d9f850ec55b67f
Wan9hLin <wanghl0724@gmail.com>
2025-12-28 21:10:51 -0500

    add interaction component & game mode & player controller

---
+++ Source/ActionRoguelike/Player/RogueInteractionComponent.cpp
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RogueInteractionComponent.h"
+
+
+// Sets default values for this component's properties
+URogueInteractionComponent::URogueInteractionComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+	
+}
+
+
+// Called every frame
+void URogueInteractionComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	
+	APlayerController* PC = CastChecked<APlayerController>(GetOwner());
+	
+	FVector Center = PC->GetPawn()->GetActorLocation();
+	
+	DrawDebugBox(GetWorld(), Center, FVector(20.0f), FColor::Red);
+}
+
