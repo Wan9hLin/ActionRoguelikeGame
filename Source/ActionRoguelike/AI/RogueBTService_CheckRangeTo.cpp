b715c3f7e4b89ab048c98d9801c8070584968aa0
Henry <wanghl0724@gmail.com>
2026-02-16 17:06:46 -0500

    add aiService

---
+++ Source/ActionRoguelike/AI/RogueBTService_CheckRangeTo.cpp
@@ -0,0 +1,11 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RogueBTService_CheckRangeTo.h"
+
+void URogueBTService_CheckRangeTo::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+	
+	
+}
---
+++ Source/ActionRoguelike/AI/RogueBTService_CheckRangeTo.h
@@ -0,0 +1,18 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTService.h"
+#include "RogueBTService_CheckRangeTo.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTIONROGUELIKE_API URogueBTService_CheckRangeTo : public UBTService
+{
+	GENERATED_BODY()
+	
+	virtual void TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+};
