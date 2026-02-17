0aa1b34b115f7debd5de668978dc18f1c9886eb4
Henry <wanghl0724@gmail.com>
2026-02-16 21:26:28 -0500

    Behavior Tree Service
    CREATE Behavior Tree Service. Services run periodically, to check the distance to the target

--- Source/ActionRoguelike/AI/RogueBTService_CheckRangeTo.cpp
+++ Source/ActionRoguelike/AI/RogueBTService_CheckRangeTo.cpp
@@ -2,10 +2,33 @@
 
 
 #include "RogueBTService_CheckRangeTo.h"
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
 void URogueBTService_CheckRangeTo::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
 {
 	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
 	
+	UBlackboardComponent* BBComp = OwnerComp.GetBlackboardComponent();
+	check(BBComp);
+	
+	AActor* TargetActor = Cast<AActor>(BBComp->GetValueAsObject(TargetActorKey.SelectedKeyName));
+	if (TargetActor)
+	{
+		AAIController* Controller = OwnerComp.GetAIOwner();
+		APawn* OwningPawn = Controller->GetPawn();
+		check(OwningPawn);
+	
+		float DistanceTo = FVector::Dist(TargetActor->GetActorLocation(), OwningPawn->GetActorLocation());
+	
+		bool bWithinRange = DistanceTo < MaxAttackRange;
+		
+		bool bHasLOs = Controller->LineOfSightTo(TargetActor);
+		
+		BBComp->SetValueAsBool(WithinRangeKey.SelectedKeyName, (bWithinRange && bHasLOs));
+	}
+	
+	
+
 	
 }
--- Source/ActionRoguelike/AI/RogueBTService_CheckRangeTo.h
+++ Source/ActionRoguelike/AI/RogueBTService_CheckRangeTo.h
@@ -14,5 +14,14 @@ class ACTIONROGUELIKE_API URogueBTService_CheckRangeTo : public UBTService
 {
 	GENERATED_BODY()
 	
+	UPROPERTY(EditAnywhere, Category="AI")
+	FBlackboardKeySelector TargetActorKey;
+	
+	UPROPERTY(EditAnywhere, Category="AI")
+	FBlackboardKeySelector WithinRangeKey;
+	
+	UPROPERTY(EditAnywhere, Category="AI")
+	float MaxAttackRange = 500;
+	
 	virtual void TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
 };
