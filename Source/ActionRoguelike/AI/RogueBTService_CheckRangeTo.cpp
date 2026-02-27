a64b9be2183e1c0a2403929fa3a40540bfac3f43
Henry <wanghl0724@gmail.com>
2026-02-26 21:45:15 -0500

    implement an EQS context

--- Source/ActionRoguelike/AI/RogueBTService_CheckRangeTo.cpp
+++ Source/ActionRoguelike/AI/RogueBTService_CheckRangeTo.cpp
@@ -3,7 +3,15 @@
 
 #include "RogueBTService_CheckRangeTo.h"
 #include "AIController.h"
+#include "RogueGameTypes.h"
 #include "BehaviorTree/BlackboardComponent.h"
+
+
+URogueBTService_CheckRangeTo::URogueBTService_CheckRangeTo()
+{
+	TargetActorKey.SelectedKeyName = NAME_TargetActor;
+}
+
 
 void URogueBTService_CheckRangeTo::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
 {
@@ -32,3 +40,4 @@ void URogueBTService_CheckRangeTo::TickNode(UBehaviorTreeComponent& OwnerComp, u
 
 	
 }
+
